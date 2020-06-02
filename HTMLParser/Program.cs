cdc49b051318f8b2ef986390c537289636c9ebd0
Scottishcoder <owaingdwilliams@gmail.com>
2020-06-02 16:32:08 +0100

    Adding comments
    Adding comments to the code to remind me of what I still need to do.

--- HTMLParser/Program.cs
+++ HTMLParser/Program.cs
@@ -17,15 +17,15 @@ namespace HTMLParser
     {
         async static Task Main(string[] args)
         {
-                        
+                 
+            // Get list of urls from an RSS feed.
             var listOfUrls = await ReadRssFeedAsync();
             
-            
-            
-            
+
             HttpClient client = new HttpClient();
             HtmlDocument pageDocument = new HtmlDocument();
-
+           
+            // Configure how to handle the HTML and how to parse it.
             var config = new ReverseMarkdown.Config
             {
                 UnknownTags = ReverseMarkdown.Config.UnknownTagsOption.Bypass, // Include the unknown tag completely in the result (default as well)
@@ -35,14 +35,16 @@ namespace HTMLParser
             };
 
 
+            // WIP.
             foreach (var link in listOfUrls)
             {
                 var temp = link;
             }
 
 
-            var converter = new ReverseMarkdown.Converter(config);
 
+            // GetAsync will be the Link returned from the RSS feed.
+            var converter = new ReverseMarkdown.Converter(config);
             var response = await client.GetAsync("https://owain.codes/blog/posts/2020/march/umbraco-mvp-heather-floyd/");
             var pageContents = await response.Content.ReadAsStringAsync();
 
@@ -55,8 +57,14 @@ namespace HTMLParser
             string html = blogPostContent;
             string blogPostMarkdown = converter.Convert(html);
 
+
+            //Instead of outputting to the console. Save as Markdown file. 
             Console.WriteLine(blogPostMarkdown);
             Console.ReadLine();
+
+            // TODO:
+            // Once all files are saved, git push them to a private repo. 
+            //
         }
 
         public static async Task<List<string>> ReadRssFeedAsync()
