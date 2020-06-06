d3ec8a7126620ec767fd63001747de4787fd53de
Louis Jackson-Rees <louisjacksonrees@gmail.com>
2020-06-06 16:08:39 +0100

    Added multi-targeting

--- HTMLParser.ConsoleApp/Program.cs
+++ HTMLParser.ConsoleApp/Program.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using HTMLParser.Processing.Processors;
-using System;
 
 namespace HTMLParser.ConsoleApp
 {
