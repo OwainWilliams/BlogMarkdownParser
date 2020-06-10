c9660d5f17090b31992102f6ad9d83bd73bf9cf9
Scottishcoder <owaingdwilliams@gmail.com>
2020-06-10 11:20:40 +0100

    Adding AppSettings.json

--- HTMLParser.ConsoleApp/Program.cs
+++ HTMLParser.ConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using HTMLParser.Processing.Processors;
+using Microsoft.Extensions.Configuration;
 
 namespace HTMLParser.ConsoleApp
 {
@@ -9,10 +10,20 @@ namespace HTMLParser.ConsoleApp
     {
         static async Task Main(string[] args)
         {
+
+            IConfiguration _configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
+
+            var rssFeedUrl = _configuration.GetValue<string>("RssFeedUrlKey");
+            var localExportPath = _configuration.GetValue<string>("LocalExportPath");
+
             // Get list of urls from an RSS feed.
-            List<string> listOfLinks = await new RssFeedProcessor("https://owain.codes/blog/rss/").GetFeedLinksAsync();
+            List<string> listOfLinks = await new RssFeedProcessor(rssFeedUrl).GetFeedLinksAsync();
             Console.WriteLine("Started.....");
-            await new HtmlProcessor(@"c:\temp\markdown\").ProcessLinks(listOfLinks);
+            await new HtmlProcessor(localExportPath).ProcessLinks(listOfLinks);
            
             // TODO:
             // Once all files are saved, git push them to a private repo. 
